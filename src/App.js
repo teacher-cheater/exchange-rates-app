f88e7ccf6bb41a84ddcb00804299a862b0fa197b
teacher_cheater <johnnykayfun@gmail.com>
2022-08-30 21:05:17 +0300

    add component input choice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './header.module.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [inputTextValue, setInputTextValue] = useState('');
+    let [, setSelectOpinion] = useState('');
+
+    return (
+        <div className="App">
+            <header>
+                <h1 className={s.header__title}> Exchange rates </h1>
+                <form className={s.header}>
+                    <div className={s.header__block}>
+                        <label className={s.header__choice}>
+                            <div className={s.header__subtext}> Choice rates </div>
+                            <select name="rates" className={s.header__rates}
+                                    onChange={(event) => setSelectOpinion(event.target.value)}>
+                                <option value="" defaultValue >
+                                    Choose one
+                                </option>
+                                <option value="Gryffindor"> Gryffindor</option>
+                                <option value="Slytherin">Slytherin</option>
+                                <option value="Hufflepuff">Hufflepuff</option>
+                                <option value="Ravenclaw">Ravenclaw</option>
+                            </select>
+                        </label>
+                        <label className={s.header__inpt}>
+                            <div className={s.header__subtext}> Поле для ввода </div>
+                            <input
+                                placeholder="100.00"
+                                className={s.header__input}
+                                type="number"
+                                value={inputTextValue}
+                                onChange={(event) => setInputTextValue(event.target.value)}
+                            />
+                        </label>
+                    </div>
+
+                    <div className="header__block-choice-2">
+                        {/*<label className="header__school-choice">*/}
+                        {/*    <p>School</p>*/}
+                        {/*    <select name="school" className="header__school"*/}
+                        {/*            onChange={(event) => setSelectOpinion(event.target.value)}>*/}
+                        {/*        <option value="" defaultValue select='true'>*/}
+                        {/*            Choose one*/}
+                        {/*        </option>*/}
+                        {/*        <option value="Gryffindor"> Gryffindor</option>*/}
+                        {/*        <option value="Slytherin">Slytherin</option>*/}
+                        {/*        <option value="Hufflepuff">Hufflepuff</option>*/}
+                        {/*        <option value="Ravenclaw">Ravenclaw</option>*/}
+                        {/*    </select>*/}
+                        {/*</label>*/}
+                        {/*<label className="header__name">*/}
+                        {/*    <p>Name</p>*/}
+                        {/*    <input*/}
+                        {/*        placeholder="100.00"*/}
+                        {/*        className="header__input-text"*/}
+                        {/*        type="number"*/}
+                        {/*        value={inputTextValue}*/}
+                        {/*        onChange={(event) => setInputTextValue(event.target.value)}*/}
+                        {/*    />*/}
+                        {/*</label>*/}
+                    </div>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
